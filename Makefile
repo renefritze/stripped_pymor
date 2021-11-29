32cf5991cf9f68096f4c5cf1eaf5b981ac0f7245
René Fritze <rene.fritze@wwu.de>
2021-11-29 14:14:00 +0100

    infra

---
+++ Makefile
@@ -0,0 +1,122 @@
+#!/usr/bin/env make
+
+# customization points via makefile key-value arguments
+#
+# interpreter in images: 3.{6,7,8} currently available
+# DOCKER_BASE_PYTHON=3.7
+# test script executed with `docker_test`: mpi, notebooks_dir, oldest, vanilla, mpi, numpy_git, pip_installed
+# PYMOR_TEST_SCRIPT=vanilla
+# version pinned mirror to be used: stable or oldest
+# PYPI_MIRROR=stable
+# wheel check OS: debian_buster centos_8 debian_testing
+# PYMOR_TEST_OS=debian_buster
+# hypothesis profiles: dev, debug, ci, ci-pr, ci-large
+# PYMOR_HYPOTHESIS_PROFILE=dev
+# extra options to be passed to pytest
+# PYMOR_PYTEST_EXTRA="--lf"
+
+THIS_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+DOCKER_RUN=docker run -v $(THIS_DIR):/pymor --env-file  $(THIS_DIR)/.env
+CI_COMMIT_REF_NAME?=$(shell git rev-parse --abbrev-ref HEAD)
+DOCKER_COMPOSE=CI_COMMIT_SHA=$(shell git log -1 --pretty=format:"%H") \
+  	CI_COMMIT_REF_NAME=$(CI_COMMIT_REF_NAME) \
+	NB_USER=$(NB_USER) $(COMPOSE_SUDO) docker-compose -f .binder/docker-compose.yml -p pymor
+NB_DIR=docs/source
+NB_USER:=${USER}
+ifeq ($(PYMOR_SUDO), 1)
+	COMPOSE_SUDO:=sudo -E
+else
+	COMPOSE_SUDO:=
+endif
+
+PANDOC_MAJOR=$(shell ( which pandoc && pandoc --version | head  -n1 | cut -d ' ' -f 2 | cut -d '.' -f 1)) || echo "pandoc missing")
+ifeq ($(PANDOC_MAJOR),1)
+	PANDOC_FORMAT=-f markdown_github
+endif
+# this loads $(ENV_FILE) as both makefile variables and into shell env
+ENV_FILE?=.env
+include $(ENV_FILE)
+export $(shell sed 's/=.*//' $(ENV_FILE))
+
+.PHONY: docker README.html pylint test docs
+
+all:
+	./dependencies.py
+
+# I want HTML (to preview the formatting :))
+README.html: README.md
+	pandoc $(PANDOC_FORMAT) -t html $< > $@
+
+README: README.html
+
+pep8:
+	pep8 ./src
+
+flake8:
+	flake8 ./src
+
+test:
+	python setup.py pytest
+
+jupyter:
+	jupyter notebook --notebook-dir=$(NB_DIR) --NotebookApp.disable_check_xsrf=True
+
+tutorials: NB_DIR="docs/_build/html"
+tutorials: docs jupyter
+
+full-test:
+	@echo
+	@echo "Ensuring that all required pytest plugins are installed ..."
+	@echo "--------------------------------------------------------------------------------"
+	@echo
+	pip install pytest-flakes
+	pip install pytest-pep8
+	pip install pytest-cov
+	@echo
+	@echo "--------------------------------------------------------------------------------"
+	@echo
+	py.test --flakes --pep8 --cov --cov-config=setup.cfg --cov-report=html --cov-report=xml src/pymortests
+
+docs:
+	PYTHONPATH=${PWD}/src/:${PYTHONPATH} make -C docs html
+
+template:
+	./dependencies.py
+	./.ci/gitlab/template.ci.py
+
+# docker targets
+docker_template:
+	@$(DOCKER_RUN) pymor/ci_sanity:$(CI_IMAGE_TAG) /pymor/dependencies.py \
+	  || $(DOCKER_RUN) pymor/ci_sanity:latest /pymor/dependencies.py
+	@$(DOCKER_RUN) pymor/ci_sanity:$(CI_IMAGE_TAG) /pymor/.ci/gitlab/template.ci.py ${GITLAB_TOKEN} \
+	  || $(DOCKER_RUN) pymor/ci_sanity:latest /pymor/.ci/gitlab/template.ci.py ${GITLAB_TOKEN}
+	@echo Files changed:
+	@git diff --name-only
+
+docker_image:
+	$(DOCKER_COMPOSE) build
+
+docker_docs: docker_image
+	NB_DIR=notebooks $(DOCKER_COMPOSE) run jupyter ./.ci/gitlab/test_docs.bash
+
+docker_run: docker_image
+	$(DOCKER_COMPOSE) run --service-ports pytest bash
+
+docker_exec: docker_image
+	$(DOCKER_COMPOSE) run --service-ports pytest bash -l -c "${DOCKER_CMD}"
+
+docker_tutorials: NB_DIR=docs/_build/html
+docker_tutorials: docker_docs docker_jupyter
+
+docker_test: docker_image
+	PYMOR_TEST_SCRIPT=$(PYMOR_TEST_SCRIPT) $(DOCKER_COMPOSE) up pytest
+
+docker_test_oldest: docker_image
+	PYMOR_TEST_SCRIPT=oldest PYPI_MIRROR=oldest DOCKER_BASE_PYTHON=3.7 $(DOCKER_COMPOSE) up pytest
+
+docker_jupyter: docker_image
+	NB_DIR=$(NB_DIR) $(DOCKER_COMPOSE) up jupyter
+docker_wheel_check: docker_image
+	PYMOR_TEST_OS=$(PYMOR_TEST_OS) $(DOCKER_COMPOSE) run --service-ports wheel_check bash
+docker_install_check: docker_image
+	PYMOR_TEST_OS=$(PYMOR_TEST_OS) $(DOCKER_COMPOSE) run --service-ports install_check bash
---
+++ src/pymor/neural_network.py
@@ -0,0 +1,203 @@
+# This file is part of the pyMOR project (https://www.pymor.org).
+# Copyright 2013-2021 pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
+"""Remark on the documentation:
+
+Due to an issue in autoapi, the classes `NeuralNetworkStatefreeOutputReductor`,
+`NeuralNetworkInstationaryReductor`, `NeuralNetworkInstationaryStatefreeOutputReductor`,
+`EarlyStoppingScheduler` and `CustomDataset` do not appear in the documentation,
+see https://github.com/pymor/pymor/issues/1343.
+"""
+
+from pymor.config import config
+
+
+if config.HAVE_TORCH:
+    from numbers import Number
+
+    import numpy as np
+
+    import torch
+    import torch.nn as nn
+    import torch.optim as optim
+    import torch.utils as utils
+
+
+    class NeuralNetworkReductor:
+        """Reduced Basis reductor relying on artificial neural networks.
+
+        This is a reductor that constructs a reduced basis using proper
+        orthogonal decomposition and trains a neural network that approximates
+        the mapping from parameter space to coefficients of the full-order
+        solution in the reduced basis.
+        The approach is described in :cite:`HU18`.
+
+        Parameters
+        ----------
+        fom
+            The full-order |Model| to reduce.
+        training_set
+            Set of |parameter values| to use for POD and training of the
+            neural network.
+        validation_set
+            Set of |parameter values| to use for validation in the training
+            of the neural network.
+        validation_ratio
+            Fraction of the training set to use for validation in the training
+            of the neural network (only used if no validation set is provided).
+        basis_size
+            Desired size of the reduced basis. If `None`, rtol, atol or l2_err must
+            be provided.
+        rtol
+            Relative tolerance the basis should guarantee on the training set.
+        atol
+            Absolute tolerance the basis should guarantee on the training set.
+        l2_err
+            L2-approximation error the basis should not exceed on the training
+            set.
+        pod_params
+            Dict of additional parameters for the POD-method.
+        ann_mse
+            If `'like_basis'`, the mean squared error of the neural network on
+            the training set should not exceed the error of projecting onto the basis.
+            If `None`, the neural network with smallest validation error is
+            used to build the ROM.
+            If a tolerance is prescribed, the mean squared error of the neural
+            network on the training set should not exceed this threshold.
+            Training is interrupted if a neural network that undercuts the
+            error tolerance is found.
+        """
+
+        def __init__(self, fom, training_set, validation_set=None, validation_ratio=0.1,
+                     basis_size=None, rtol=0., atol=0., l2_err=0., pod_params=None,
+                     ann_mse='like_basis'):
+            assert 0 < validation_ratio < 1 or validation_set
+
+        def reduce(self, hidden_layers='[(N+P)*3, (N+P)*3]', activation_function=torch.tanh,
+                   optimizer=optim.LBFGS, epochs=1000, batch_size=20, learning_rate=1.,
+                   restarts=10, seed=0):
+            """Reduce by training artificial neural networks.
+
+            Parameters
+            ----------
+            hidden_layers
+                Number of neurons in the hidden layers. Can either be fixed or
+                a Python expression string depending on the reduced basis size
+                respectively output dimension `N` and the total dimension of
+                the |Parameters| `P`.
+            activation_function
+                Activation function to use between the hidden layers.
+            optimizer
+                Algorithm to use as optimizer during training.
+            epochs
+                Maximum number of epochs for training.
+            batch_size
+                Batch size to use if optimizer allows mini-batching.
+            learning_rate
+                Step size to use in each optimization step.
+            restarts
+                Number of restarts of the training algorithm. Since the training
+                results highly depend on the initial starting point, i.e. the
+                initial weights and biases, it is advisable to train multiple
+                neural networks by starting with different initial values and
+                choose that one performing best on the validation set.
+            seed
+                Seed to use for various functions in PyTorch. Using a fixed seed,
+                it is possible to reproduce former results.
+
+            Returns
+            -------
+            rom
+                Reduced-order |NeuralNetworkModel|.
+            """
+
+    class NeuralNetworkStatefreeOutputReductor:
+        """Output reductor relying on artificial neural networks.
+
+        This is a reductor that trains a neural network that approximates
+        the mapping from parameter space to output space.
+
+        Parameters
+        ----------
+        fom
+            The full-order |Model| to reduce.
+        training_set
+            Set of |parameter values| to use for POD and training of the
+            neural network.
+        validation_set
+            Set of |parameter values| to use for validation in the training
+            of the neural network.
+        validation_ratio
+            Fraction of the training set to use for validation in the training
+            of the neural network (only used if no validation set is provided).
+        validation_loss
+            The validation loss to reach during training. If `None`, the neural
+            network with the smallest validation loss is returned.
+        """
+
+        def __init__(self, fom, training_set, validation_set=None, validation_ratio=0.1,
+                     validation_loss=None):
+            assert 0 < validation_ratio < 1 or validation_set
+
+        def _compute_layer_sizes(self, hidden_layers):
+            """Compute the number of neurons in the layers of the neural network."""
+            # determine the numbers of neurons in the hidden layers
+            if isinstance(hidden_layers, str):
+                hidden_layers = eval(hidden_layers, {'N': self.fom.dim_output, 'P': self.fom.parameters.dim})
+            # input and output size of the neural network are prescribed by the
+            # dimension of the parameter space and the output dimension
+            assert isinstance(hidden_layers, list)
+            return [self.fom.parameters.dim, ] + hidden_layers + [self.fom.dim_output, ]
+
+
+    class NeuralNetworkInstationaryReductor(NeuralNetworkReductor):
+        """Reduced Basis reductor for instationary problems relying on artificial neural networks.
+
+        This is a reductor that constructs a reduced basis using proper
+        orthogonal decomposition and trains a neural network that approximates
+        the mapping from parameter and time space to coefficients of the
+        full-order solution in the reduced basis.
+        The approach is described in :cite:`WHR19`.
+
+        Parameters
+        ----------
+        fom
+            The full-order |Model| to reduce.
+        training_set
+            Set of |parameter values| to use for POD and training of the
+            neural network.
+        validation_set
+            Set of |parameter values| to use for validation in the training
+            of the neural network.
+        validation_ratio
+            Fraction of the training set to use for validation in the training
+            of the neural network (only used if no validation set is provided).
+        basis_size
+            Desired size of the reduced basis. If `None`, rtol, atol or l2_err must
+            be provided.
+        rtol
+            Relative tolerance the basis should guarantee on the training set.
+        atol
+            Absolute tolerance the basis should guarantee on the training set.
+        l2_err
+            L2-approximation error the basis should not exceed on the training
+            set.
+        pod_params
+            Dict of additional parameters for the POD-method.
+        ann_mse
+            If `'like_basis'`, the mean squared error of the neural network on
+            the training set should not exceed the error of projecting onto the basis.
+            If `None`, the neural network with smallest validation error is
+            used to build the ROM.
+            If a tolerance is prescribed, the mean squared error of the neural
+            network on the training set should not exceed this threshold.
+            Training is interrupted if a neural network that undercuts the
+            error tolerance is found.
+        """
+
+        def __init__(self, fom, training_set, validation_set=None, validation_ratio=0.1,
+                     basis_size=None, rtol=0., atol=0., l2_err=0., pod_params=None,
+                     ann_mse='like_basis'):
+            assert 0 < validation_ratio < 1 or validation_set
+
